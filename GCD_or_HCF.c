04cb56987b0d51bea01f1d701b78a16760e817e6
22MH1A05F7 <119723363+22MH1A05F7@users.noreply.github.com>
2022-12-24 01:11:37 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for(i=a<b?a:b;i>=1;i--)     if(a%i==0 && b%i==0)     {         break;     }     printf("%d",i); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    for(i=a<b?a:b;i>=1;i--)
+    if(a%i==0 && b%i==0)
+    {
+        break;
+    }
+    printf("%d",i);
+}
