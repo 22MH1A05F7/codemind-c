1024714c4d8f557916e9c087c50d732b2ebb3972
22MH1A05F7 <119723363+22MH1A05F7@users.noreply.github.com>
2023-01-06 22:45:21 +0530

    #include<stdio.h> int main() {     char str[100];     scanf("%[^ ]s",str);     int i,j=0,k=0,m=0,s=0;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')         {             j++;         }         else if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))         {             k++;         }         else if(str[i]>='0'&& str[i]<='9')         {            m++;         }         else if(str[i]==' ')         {             s++;         }     }     printf("Vowels: %d Consonants: %d Digits: %d White spaces: %d",j,k,m,s); }

---
+++ Count_no._of_vowels_,consonants_,digits_and_spaces.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    scanf("%[^
+]s",str);
+    int i,j=0,k=0,m=0,s=0;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+        {
+            j++;
+        }
+        else if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))
+        {
+            k++;
+        }
+        else if(str[i]>='0'&& str[i]<='9')
+        {
+           m++; 
+        }
+        else if(str[i]==' ')
+        {
+            s++;
+        }
+    }
+    printf("Vowels: %d
+Consonants: %d
+Digits: %d
+White spaces: %d",j,k,m,s);
+}
